720be127d9f2fb9c1b8132f61c217c105b7b332a
MD-Nayeem909 <mdnayeemuddin909@gmail.com>
2025-11-12 19:04:53 +0600

    Hero section upgraded

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Link } from "react-router";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import image1 from "../assets/food-menu-1.jpg";
+import image2 from "../assets/food-menu-2.jpg";
+import image3 from "../assets/food-menu-3.jpg";
 
 // Import Swiper styles
 import "swiper/css";
@@ -27,48 +30,78 @@ const Hero = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <section className="hero bg-linear-to-tl from-[#E0F8F5] to-[#FFE6FD] to-100% p-20 rounded-lg flex flex-col justify-between items-center gap-8 ">
-            <div className="text-center space-y-4">
-              <h2 className="text-7xl font-bold">
-                Deal Your <span className="text-gradient">Products</span>
-                <br />
-                In A <span className="text-gradient">Smart</span> Way !
-              </h2>
-              <p className="text-accent text-xl">
-                SmartDeals helps you sell, resell, and shop from trusted local
-                sellers — all in one place!
-              </p>
-            </div>
-            <div className="flex mx-auto shadow-lg rounded-full overflow-hidden">
-              <div className=" flex w-[500px]">
-                <label className="input rounded-l-full rounded-r-none w-full border-none">
-                  <input
-                    type="text"
-                    placeholder="search For Products, Categoriees..."
-                    className=""
-                  />
-                </label>
+          <section className="hero bg-linear-to-tl from-[#E0F8F5] to-[#FFE6FD] to-100% lg:p-20 rounded-lg">
+            <div className="flex flex-col justify-between items-center gap-4 lg:gap-8 p-10">
+              <div className="text-center space-y-4">
+                <h2 className="text-3xl lg:text-7xl font-bold">
+                  Deal Your <span className="text-gradient">Products</span>
+                  <br />
+                  In A <span className="text-gradient">Smart</span> Way !
+                </h2>
+                <p className="text-accent lg:text-xl">
+                  SmartDeals helps you sell, resell, and shop from trusted local
+                  sellers — all in one place!
+                </p>
               </div>
-              <button className="btn btn-primary px-3! rounded-r-full! rounded-l-none!">
-                <Search />
-              </button>
+              <div className="flex mx-auto shadow-lg rounded-full w-full max-w-2xl">
+                <div className="flex w-full">
+                  <label className="input rounded-l-full rounded-r-none border-none w-full">
+                    <input
+                      type="text"
+                      placeholder="search For Products, Categories..."
+                      className="p-3 border-none"
+                    />
+                  </label>
+                </div>
+                <button className="btn btn-primary px-3! rounded-r-full! rounded-l-none!">
+                  <Search />
+                </button>
+              </div>
+              <div className="gap-4 flex">
+                <Link to="/all-products" className="btn btn-primary">
+                  Watch All Reviews
+                </Link>
+                <Link to="/create-product" className="btn btn-outline-gradient">
+                  Add Review
+                </Link>
+              </div>
             </div>
-            <div className="gap-4 flex">
-              <Link to="/all-products" className="btn btn-primary">
-                Watch All Reviews
-              </Link>
-              <Link to="/create-product" className="btn btn-outline-gradient">
-                Add Review
-              </Link>
+          </section>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <section className=" py-20 rounded-lg ">
+            <div className="w-full">
+              <img
+                src={image1}
+                alt=""
+                className="object-cover overflow-hidden w-full"
+              />
             </div>
           </section>
         </SwiperSlide>
         <SwiperSlide>
-          <section className="hero bg-linear-to-tl from-[#E0F8F5] to-[#FFE6FD] to-100% p-20 rounded-lg flex flex-col justify-between items-center gap-8">
-            Slide 2
+          <section className=" py-20 rounded-lg ">
+            <div className="w-full">
+              <img
+                src={image2}
+                alt=""
+                className="object-cover overflow-hidden w-full"
+              />
+            </div>
           </section>
         </SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>
+          <section className=" py-20 rounded-lg ">
+            <div className="w-full">
+              <img
+                src={image3}
+                alt=""
+                className="object-cover overflow-hidden w-full"
+              />
+            </div>
+          </section>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
